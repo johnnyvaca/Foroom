1d70df1a0d3bede9018bc5b12890fa1e63d0712c
yogo <47849728+johnnyvaca@users.noreply.github.com>
2021-02-10 08:27:24 +0100

    categories_activity created

---
+++ app/src/main/java/com/example/foroom/Activity_Categories.java
@@ -0,0 +1,31 @@
+package com.example.foroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity_Categories extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+
+        Intent intent = new Intent(this,Activity_Topics.class);
+
+
+
+        Button button = findViewById(R.id.buttonTopics);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+    }
+}
