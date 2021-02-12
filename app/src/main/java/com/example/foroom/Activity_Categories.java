0ef0401c10d3af7301aa9a2a12f1193495ae8100
yogo <47849728+johnnyvaca@users.noreply.github.com>
2021-02-12 07:47:50 +0100

    Revert "DataProvider WIP"
    This reverts commit a4892079244357e543128424676c69b014ad79ff.

--- app/src/main/java/com/example/foroom/Activity_Categories.java
+++ app/src/main/java/com/example/foroom/Activity_Categories.java
@@ -7,18 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.google.android.material.transition.SlideDistanceProvider;
-
 public class Activity_Categories extends AppCompatActivity {
 
-  
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
 
-        Intent intent = new Intent(this, Activity_Topics.class);
+        Intent intent = new Intent(this,Activity_Topics.class);
+
 
 
         Button button = findViewById(R.id.buttonTopics);
@@ -28,13 +27,5 @@ public class Activity_Categories extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-      DataProvider instance = DataProvider.getInstance();
-
-
-    Categories category = instance.getObjectCategories(this);
-
-
-
     }
 }
