4e7253785443a724a1b66952f5d951c93f7fe8f2
yogo <47849728+johnnyvaca@users.noreply.github.com>
2021-02-12 10:38:27 +0100

    okey

--- app/src/main/java/com/example/foroom/Activity_Categories.java
+++ app/src/main/java/com/example/foroom/Activity_Categories.java
@@ -5,8 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*;
-
+import android.widget.Button;
 
 public class Activity_Categories extends AppCompatActivity {
 
@@ -15,8 +14,8 @@ public class Activity_Categories extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Test test = new Test();
+        setContentView(R.layout.activity_categories);
+
         Intent intent = new Intent(this,Activity_Topics.class);
 
 
@@ -28,6 +27,5 @@ public class Activity_Categories extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
     }
 }
